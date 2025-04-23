1b547dfb59d178eb800e2d4add97ab79428d4688
JuanDavid <77819591+JuanDpr99@users.noreply.github.com>
2025-04-23 12:32:04 -0500

    Primer commit de proyecto C#

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Calculadora
+{
+    static void Main()
+    {
+        double num1, num2, resultado;
+        char operacion;
+
+        Console.WriteLine("=== CALCULADORA BÁSICA ===");
+
+        Console.Write("Ingrese el primer número: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el operador (+, -, *, /): ");
+        operacion = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        switch (operacion)
+        {
+            case '+':
+                resultado = num1 + num2;
+                Console.WriteLine($"Resultado: {resultado}");
+                break;
+            case '-':
+                resultado = num1 - num2;
+                Console.WriteLine($"Resultado: {resultado}");
+                break;
+            case '*':
+                resultado = num1 * num2;
+                Console.WriteLine($"Resultado: {resultado}");
+                break;
+            case '/':
+                if (num2 != 0)
+                {
+                    resultado = num1 / num2;
+                    Console.WriteLine($"Resultado: {resultado}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: División por cero.");
+                }
+                break;
+            default:
+                Console.WriteLine("Operador no válido.");
+                break;
+        }
+
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
